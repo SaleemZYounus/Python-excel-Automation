6d1a182c32e8e092af88c0557efa28f09bfb2db5
SaleemZYounus <51415891+SaleemZYounus@users.noreply.github.com>
2022-07-26 17:23:19 -0500

    Add files via upload

--- Python-openpyxl.py
+++ Python-openpyxl.py
@@ -1,9 +1,8 @@
 import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
 
 wb = xl.load_workbook('transactions.xlsx')
 sheet = wb['Sheet1']
-cell = sheet['a1']
-cell = sheet.cell(1,1)
 
 
 print(sheet.max_row)
@@ -13,7 +12,20 @@ for row in range(2, sheet.max_row + 1):
     cell = sheet.cell(row, 3)
     print(cell.value)
     corrected_price = cell.value * .9
-    corrected_cell = sheet.cell(row, 4)
+    corrected_cell = sheet.cell(row, 4) #creates new row
     corrected_cell.value = corrected_price
+
+sheet["d1"].value = "new price" #adds title to D1
+
+values = Reference(sheet, 
+        min_row = 2, 
+        max_row = sheet.max_row,
+        min_col = 4,
+        max_col = 4)
+
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, 'e2')
+
 
 wb.save('transactions2.xlsx')
